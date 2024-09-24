69f9151e491ab07892ea4676b44e50312c73ffdc
gaby18393 <aleixo.gabrielly@escola.pr.gov.br>
2024-09-24 10:50:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+criarCartao  (categoria,pergunta,resposta ) (
 
+)
+criarCartao( categoria,pergunta,resposta)(
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+)
+cartao.innerHTML = 
+`<div class="cartao_conteudo_pergunta"> 
+<p> O que é JavaScript?</p>
+</div>
+<div class="cartao_conteudo_resposta">
+<p> O Java Script é uma liguagem da programação</p>`
+CSSContainerRule.appenChild(cartao)
